060d4f15af83614f99607f9c0510e518b39aa0c1
Mark Orel <mail.ormark@gmail.com>
2025-07-04 17:26:33 +0100

    ex 1.1.6

---
+++ src/nature_of_code/exercises/1_1_6_noise_acceleration.clj
@@ -0,0 +1,38 @@
+(ns nature-of-code.exercises.1-1-6-noise-acceleration
+  (:require [quil.core :as q])
+  (:require [nature-of-code.utils :as u])
+  (:require [mikera.vectorz.core :as m]))
+
+(def OFFSET 9)
+(defn setup []
+  (q/frame-rate 60)
+  (q/background 230)
+  (q/color-mode :hsb)
+  (q/fill 0)
+  {:counter 0
+   :tx 0
+   :ty 100
+   :position (m/vec2 100 100)
+   :acc (m/vec2 -0.1 0.1)
+   :velocity (m/vec2 (q/random -1 1) (q/random -1 1))})
+
+(defn update-state [{:keys [acc position velocity counter tx ty] :as state}]
+  (let [[vx vy] (m/to-list velocity)
+        [px py] (m/to-list position)
+        mouse-n (m/normalise (u/mouse-vec))
+        x-off (u/offset OFFSET q/width px)
+        y-off (u/offset OFFSET q/height py)
+        new-acc (u/limit (m/vec2 (* (q/noise tx) (q/random -2 2)) (* (q/noise ty) (q/random -1 1))) 5)
+        new-velocity (m/vec2 (* x-off vx) (* y-off vy))]
+    (q/print-every-n-millisec 1000 (u/x new-velocity) "\n-----" (u/y new-velocity))
+    {:counter (inc counter)
+     :acc (m/add mouse-n new-acc)
+     :velocity (u/limit (m/add new-velocity new-acc) 10)
+     :position (m/add position new-velocity)
+     :tx (+ 0.01 tx)
+     :ty (+ 0.01 ty)}))
+
+(defn draw [{:keys [velocity position] :as state}]
+  ; (q/print-every-n-millisec 1000 position velocity)
+  (q/background 230)
+  (q/ellipse (u/x position) (u/y position) 20 20))
--- src/nature_of_code/utils.clj
+++ src/nature_of_code/utils.clj
@@ -9,10 +9,27 @@
 
 (defn limit [v max]
   (if (> (m/magnitude v) max)
-   (m/mul (m/normalise v) max)
-   v))
+    (m/mul (m/normalise v) max)
+    v))
+
+(defn offset [off measure pos]
+  (if (or (>= pos (- (measure) off)) (<= (- pos off) 0)) -1 1))
+
+(defn sign [x]
+  (if (>= x 0)
+    1 
+    -1))
 
 (defn x [vec]
   (m/mget vec 0))
 (defn y [vec]
   (m/mget vec 1))
+
+(defn- relative-x []
+  (- (q/mouse-x) (/ (q/width) 2)))
+
+(defn- relative-y []
+  (- (q/mouse-y) (/ (q/height) 2)))
+
+(defn mouse-vec []
+  (m/vec2 (relative-x) (relative-y)))
