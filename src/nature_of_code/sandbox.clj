3dacbfa93e61846139cd49782245e16690101ca4
Mark Orel <mail.ormark@gmail.com>
2025-08-13 00:02:24 +0100

    update core and sanbox

--- src/nature_of_code/sandbox.clj
+++ src/nature_of_code/sandbox.clj
@@ -1,5 +1,6 @@
 (ns nature-of-code.sandbox
-  (:require [quil.core :as q])
+  (:require [quil.core :as q]
+            [quil.middleware :as mm])
   (:require [nature-of-code.utils :as u])
   (:require [mikera.vectorz.core :as v]))
 
@@ -17,3 +18,18 @@
 (defn draw [state]
  (q/text (str "Counter: " (:counter state)) 100 100))
 
+(defn key-pressed [state event]
+  (case (:key event)
+    :s (do (q/start-loop) (assoc state :running true))
+    :p (do (q/no-loop) (assoc state :running false))
+    :r (setup)
+    state))
+
+(q/defsketch sandbox
+  :title "Sandbox for playing with quil"
+  :setup setup
+  :update update-state
+  :draw draw
+  :key-pressed key-pressed
+  :size [500 500]
+  :middleware [mm/fun-mode mm/pause-on-error])
